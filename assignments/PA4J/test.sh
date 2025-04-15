d3203a5a48cfd6f8eba308a4c9574933ece6a204
teleivo <teleivo@users.noreply.github.com>
2025-04-15 08:12:45 +0200

    diff semant with reference impl

--- assignments/PA4J/test.sh
+++ assignments/PA4J/test.sh
@@ -11,7 +11,11 @@ fi
 
 for file in "$TEST_DIR"/*.cl; do
     if [ -f "$file" ]; then
-        if ! diff <(../../bin/lexer "$file" | ../../bin/parser | ../../bin/semant) <(../../bin/lexer "$file" | ../../bin/parser | ../../bin/semant); then
+        # Run the command and capture only stderr, then sort line number (numerically)
+        output1=$(../../bin/lexer "$file" | ../../bin/parser | ../../bin/semant 2>&1 1>/dev/null | sort -t: -k2n)
+        output2=$(../../bin/lexer "$file" | ../../bin/parser | ./semant 2>&1 1>/dev/null | sort -t: -k2n)
+
+        if ! diff <(echo "$output1") <(echo "$output2"); then
             echo "Testing $file: failed"
             failed=1
         else
